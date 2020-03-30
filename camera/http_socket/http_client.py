bb1b7c132be17d264ffc16e97e14b5a87147aead
saya-kira <slawnqiu@gmail.com>
2020-03-30 17:41:40 +0800

    make event loop of asyncio

--- camera/http_socket/http_client.py
+++ camera/http_socket/http_client.py
@@ -11,13 +11,19 @@ full_url = "http://" + ip_addr + ":" + remote_port + mp_url_path
 async def request_http(data):
     async with aiohttp.ClientSession() as session:  # open a session
         with aiohttp.MultipartWriter() as mpwriter:  # make a multipart writer
+            print("包裝 multipart 中：二進位")
             part = mpwriter.append(data)  # add a part include the photo data
             # part.set_content_disposition('binary')
             part.headers[aiohttp.hdrs.CONTENT_TYPE] = 'binary'
+            print("包裝 multipart 中：字串")
             mpwriter.append("the local size is " + str(len(data)))
             # use the default content type plain/text
+            print("送出 multipart 中，，，")
             async with session.post(full_url, data=mpwriter) as resp:
-                return resp.status, await resp.text()
+                print("收取結果中，，，")
+                code = resp.status
+                result = await resp.text()
+                return code, result
 
 
 # def read_file():  # 輸入檔案路徑，將檔案存入記憶體，返回位元組數組
@@ -36,7 +42,8 @@ async def request_http(data):
 
 def send_photo(file_stream):
     file_stream.seek(0)
-    result_code, result_text = request_http(file_stream)
+    loop = asyncio.get_event_loop()
+    result_code, result_text = loop.run_until_complete(request_http(data))
     return result_code, result_text
 
 
