a677259d063ab9af83d1d7ff48d0fe20e0fdbba9
saya-kira <slawnqiu@gmail.com>
2020-03-30 16:57:15 +0800

    加入 http 通訊機能

--- camera/camera_stream.py
+++ camera/camera_stream.py
@@ -1,13 +1,22 @@
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
+from http_socket.http_client import send_photo
 
 
-# Create an in-memory stream
-my_stream = BytesIO()
-camera = PiCamera()
-camera.start_preview()
-# Camera warm-up time
+def capture_photo():
+    # Create an in-memory stream
+    my_stream = BytesIO()
+    camera = PiCamera()
+    camera.start_preview()
+    # Camera warm-up time
 
-sleep(2)
-camera.capture(my_stream, 'jpeg')
+    sleep(2)
+    camera.capture(my_stream, 'jpeg')
+    return my_stream
+
+
+if __name__ == '__main__':
+    the_stream = capture_photo()
+    result_code, result_text = send_photo(the_stream)
+    print(result_code, result_text)
---
+++ camera/http_socket/http_client.py
@@ -0,0 +1,47 @@
+import aiohttp
+import asyncio
+import io
+
+ip_addr = "127.0.0.1"
+remote_port = "8086"
+mp_url_path = "/mp"
+full_url = "http://" + ip_addr + ":" + remote_port + mp_url_path
+
+
+async def request_http(data):
+    async with aiohttp.ClientSession() as session:  # open a session
+        with aiohttp.MultipartWriter() as mpwriter:  # make a multipart writer
+            part = mpwriter.append(data)  # add a part include the photo data
+            # part.set_content_disposition('binary')
+            part.headers[aiohttp.hdrs.CONTENT_TYPE] = 'binary'
+            mpwriter.append("the local size is " + str(len(data)))
+            # use the default content type plain/text
+            async with session.post(full_url, data=mpwriter) as resp:
+                return resp.status, await resp.text()
+
+
+# def read_file():  # 輸入檔案路徑，將檔案存入記憶體，返回位元組數組
+#     filename = input('copy your file uri here: ')
+#     filename = filename.replace('\"','',2)
+#     try:
+#         file_handle = open(filename, 'br')
+#     except Exception as esu:  # 嘗試二進位打開檔案
+#         print(esu)
+#         return None
+#     finally:
+#         data_read = bytearray(file_handle.read())
+#         file_handle.close()
+#         return data_read
+
+
+def send_photo(file_stream):
+    file_stream.seek(0)
+    result_code, result_text = request_http(the_stream)
+    return result_code, result_text
+
+
+if __name__ == '__main__':
+    data = read_file()
+    # print(len(data))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(request_http(data))
